3dbe7bbd7feb07c9699b72fb37f9bb5ea65867d5
33joe33 <joseph.oliver,]>
2022-06-09 21:36:57 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <functional>
 using namespace std;
-using namespace placeholders;
-​
-/* TODO: Write recursive DigitCount() function here. */
-int DigitCount2(int num, int count){
-    if (num<=9) return count+1;
-    else return DigitCount2(num/10, count + 1);
+string spaces = "         ";
+string stars = "*";
+/* TODO: Write recursive DrawTriangle() function here. */
+void DrawTriangle(int baseLength){
+    if (baseLength==-1) return;
+    cout<<spaces<<stars<<"\n";
+    spaces.pop_back();
+    stars.push_back('*');
+    stars.push_back('*');
+    DrawTriangle(baseLength - 2);
 }
 ​
-auto DigitCount = bind(DigitCount2, _1, 0);
-​
 int main() {
-    int num;
-    int digits;
-    ​
-    cin >> num;
-    digits = DigitCount(num);
-    cout << digits << endl;
+    int baseLength;
+
+    cin >> baseLength;
+    DrawTriangle(baseLength);
     return 0;
 }
