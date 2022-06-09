2e52db509410f823b917874d1c3179bb32b831a6
33joe33 <joseph.oliver,]>
2022-06-09 22:36:41 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,49 +1,31 @@
-#include <string>
-#include <vector>
-#include <iostream>
-​
+#include<iostream>
+#include<string>
 using namespace std;
+#ifndef STATEPAIR
+#define STATEPAIR
 ​
-// TODO: Define a generic method called CheckOrder() that
-//       takes in four variables of generic type as arguments.
-//       The return type of the method is integer
-​
-// Check the order of the input: return -1 for ascending,
-// 0 for neither, 1 for descending
-template <typename T>
-int CheckOrder(T a, T b, T c, T d){
-    vector<T> args = {b, c, d};
-    bool asc = true, desc = true;
-    T prev = a;
-    for (const auto &e:args){
-        if (e < prev) asc = false;
-        if (e > prev) desc = false;
-        prev = e;
+template<typename T1, typename T2>
+class StatePair {
+private:
+    T1 key;
+    T2 value;
+public:
+// TODO: Define constructors
+    StatePair(){}
+    StatePair(T1 userKey, T2 userValue){
+        key = userKey;
+        value = userValue;
     }
-    if (asc) return -1;
-    if (desc) return 1;
-    return 0;
-}
-​
-​
-int main() {
-    // Read in four strings
-    string stringArg1, stringArg2, stringArg3, stringArg4;
-    cin >> stringArg1;
-    cin >> stringArg2;
-    cin >> stringArg3;
-    cin >> stringArg4;
-    // Check order of four strings
-    cout << "Order: " << CheckOrder(stringArg1, stringArg2, stringArg3, stringArg4) << endl;
-    ​
-    // Read in four doubles
-    double doubleArg1, doubleArg2, doubleArg3, doubleArg4;
-    cin >> doubleArg1;
-    cin >> doubleArg2;
-    cin >> doubleArg3;
-    cin >> doubleArg4;
-    // Check order of four doubles
-    cout << "Order: " << CheckOrder(doubleArg1, doubleArg2, doubleArg3, doubleArg4) << endl;
 
-    return 0;
-}
+// TODO: Define mutators, and accessors for StatePair
+    void SetKey(T1 userKey){key = userKey;}
+    void SetValue(T2 userValue){value = userValue;}
+    T1 GetKey(){return key;}
+    T2 GetValue(){return value;}
+// TODO: Define PrintInfo() method
+    void PrintInfo(){
+        cout<<key<<": "<<value<<"\n";
+    }
+};
+​
+#endif
