8494fc31618f478cbe7b3eabcdc8e06e4d26afad
33joe33 <joseph.oliver,]>
2022-06-09 22:02:38 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,68 @@
+#include <string>
 #include <iostream>
-#include <iomanip>
 #include <stdexcept>
+#include <fstream>
 using namespace std;
 ​
-/* Define your function here */
-double StepsToMiles(int steps){
-    double steps2 = steps;
-    if (steps < 0) throw runtime_error("Exception: Negative step count entered.");
-    else return steps2/2000;
-}
-int main() {
-    int steps;
+string FindID(string name, ifstream &infoFS) {
+    string s;
     /* Type your code here. */
-    cin>>steps;
-    cout << fixed << setprecision(2);
-    try{cout<<StepsToMiles(steps)<<"\n";}
+    while(infoFS>>s){
+        if (s == name){
+            infoFS>>s;
+            return s;
+        }
+    }
+    s = "Student ID not found for "+name;
+    throw runtime_error(s);
+}
+​
+string FindName(string ID, ifstream &infoFS) {
+    string s, t;
+    /* Type your code here. */
+    while(infoFS>>s>>t){
+        if (t == ID){
+            return s;
+        }
+    }
+    s = "Student name not found for "+ID;
+    throw runtime_error(s);
+}
+​
+int main() {
+    int userChoice;
+    string studentName;
+    string studentID;
+
+    string studentInfoFileName;
+    ifstream studentInfoFS;
+
+    // Read the text file name from user
+    cin >> studentInfoFileName;
+
+    // Open the text file
+    studentInfoFS.open(studentInfoFileName);
+
+    // Read search option from user. 0: FindID(), 1: FindName()
+    cin >> userChoice;
+    ​
+    // FIXME: FindID() and FindName() may throw an Exception.
+    //        Insert a try/catch statement to catch the exception and output the exception message.
+    try{
+        if (userChoice == 0) {
+            cin >> studentName;
+            studentID = FindID(studentName, studentInfoFS);
+            cout << studentID << endl;
+        }
+        else {
+            cin >> studentID;
+            studentName = FindName(studentID, studentInfoFS);
+            cout << studentName << endl;
+        }
+    }
     catch(runtime_error &e){
         cout<<e.what()<<"\n";
     }
-
+    studentInfoFS.close();
     return 0;
 }
