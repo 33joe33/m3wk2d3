706a97746646dc7e208472750ed05cfdc268bb1c
33joe33 <joseph.oliver,]>
2022-06-09 22:13:27 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,49 @@
+#include <string>
+#include <vector>
 #include <iostream>
+​
 using namespace std;
 ​
+// TODO: Define a generic method called CheckOrder() that
+//       takes in four variables of generic type as arguments.
+//       The return type of the method is integer
+​
+// Check the order of the input: return -1 for ascending,
+// 0 for neither, 1 for descending
+template <typename T>
+int CheckOrder(T a, T b, T c, T d){
+    vector<T> args = {b, c, d};
+    bool asc = true, desc = true;
+    T prev = a;
+    for (const auto &e:args){
+        if (e < prev) asc = false;
+        if (e > prev) desc = false;
+        prev = e;
+    }
+    if (asc) return -1;
+    if (desc) return 1;
+    return 0;
+}
+​
+​
 int main() {
-    cin.exceptions(ios::failbit); // Allow cin to throw exceptions
-    int val1, val2, val3, max, count = 0;
+    // Read in four strings
+    string stringArg1, stringArg2, stringArg3, stringArg4;
+    cin >> stringArg1;
+    cin >> stringArg2;
+    cin >> stringArg3;
+    cin >> stringArg4;
+    // Check order of four strings
+    cout << "Order: " << CheckOrder(stringArg1, stringArg2, stringArg3, stringArg4) << endl;
+    ​
+    // Read in four doubles
+    double doubleArg1, doubleArg2, doubleArg3, doubleArg4;
+    cin >> doubleArg1;
+    cin >> doubleArg2;
+    cin >> doubleArg3;
+    cin >> doubleArg4;
+    // Check order of four doubles
+    cout << "Order: " << CheckOrder(doubleArg1, doubleArg2, doubleArg3, doubleArg4) << endl;
 
-    val1 = 0;
-    val2 = 0;
-    val3 = 0;
-    /* Type your code here. */
-    try{
-        cin>>val1;
-        max = val1;
-        count++;
-        cin>>val2;
-        max = (max < val2) ? val2 : max;
-        count++;
-        cin>>val3;
-        max = (max < val3) ? val3 : max;
-        count++;
-        cout<<max<<"\n";
-    }
-    catch(ios_base::failure &e){
-        cout<<count<<" input(s) read:\n";
-        if (count > 0) cout<<"Max is "<<max<<"\n";
-        else cout<<"No max\n";
-    }
     return 0;
 }
