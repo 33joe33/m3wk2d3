f068287d3f5304cde581994d7495f90a5e700eeb
33joe33 <joseph.oliver,]>
2022-06-10 11:15:06 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,38 +1,11 @@
-#include <queue>
 #include <iostream>
-​
 using namespace std;
-​
-int main () {
-    string personName = "";
-    int counter = 0;
-    int youPosition;
-    ​
-    queue<string> peopleInQueue;
-    ​
-    getline(cin, personName);
-    while (personName != "-1") {
-        // TODO: Add personName to peopleInQueue
-        //       determine position of "You" (youPosition)
-        counter++;
-        peopleInQueue.push(personName);
-        if(personName == "You") youPosition = counter;
-        getline(cin, personName);
-    }
-    ​
-    cout << "Welcome to the ticketing service... " << endl;
-    cout << "You are number " << youPosition << " in the queue." << endl;
-    ​
-    // TODO: In a loop, remove head person from peopleInQueue,
-    //       output their name and that they have purchased a ticket,
-    //       then output your position in the queue. When you are at
-    //       the head, output that you can purchase your ticket.
-    for(; youPosition > 1; youPosition--){
-        cout<<peopleInQueue.front()<<" has purchased a ticket.\n";
-        peopleInQueue.pop();
-        cout<<"You are now number "<<youPosition - 1<<"\n";
-    }
-    cout<<"You can now purchase your ticket!\n";
-    ​
+
+int main() {
+
+    int number;
+    cin>>number;
+    cout<<2*number<<endl;
+
     return 0;
 }
