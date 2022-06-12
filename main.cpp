834f7eb64fc816f481c523a6e2ce7184fd276f07
33joe33 <joseph.oliver,]>
2022-06-13 00:20:49 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
+#include <algorithm>
 #include <vector>
 int main() {
-int length;
-cin >> length;
-vector<int> numbers;
-int temp;
-    for (int i = 0; i < length; ++i) {
-        cin>>temp;
-        numbers.push_back(temp);
+    string str, sub;
+    getline(cin,str);
+    bool toggle =true;
+    auto pos=str.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",0);
+    int counter=0;
+
+    while(pos!=string::npos) {
+
+        if (toggle)
+            {  pos =str.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",pos);
+                if (pos!=string::npos)
+                    counter+=1;
+               toggle= false;
+            }
+        else
+          {
+            pos = str.find_first_of(' ',pos);
+            toggle=true;
+          }
+
     }
-    for (int i = 0; i < length-1; ++i) {
-        cout<<numbers[i]<<", ";
+    cout<<counter<<endl;
+return 0;
     }
-cout<< numbers[length-1]<<"."<<endl;
-return 0;}
