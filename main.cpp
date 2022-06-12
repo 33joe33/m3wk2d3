67e22114db9c93caa9026b4400a2a62399ff7104
33joe33 <joseph.oliver,]>
2022-06-12 20:00:47 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
 using namespace std;
-
+#include <vector>
 int main() {
-string password;
-cin>>password;
-bool let=false;
-
-bool num =false;
-bool special =false;
-
-for (auto &&letter :password){
-if (isdigit(letter))
-      num =true;
-
-if (letter =='!' or letter=='#' or letter=='%')
-    special =true;
-else
-    if (!isdigit(letter))
-        let= true;
-
-}
-    if (password.length()<8)
-        cout<<"Too short"<<endl;
-    else if( num and  special and  let)
-        cout<<"OK"<<endl;
-    if (!let)
-        cout<<"Missing letter"<<endl;
-
-    if (!num)
-        cout<<"Missing number"<<endl;
-    if (!special)
-        cout<<"Missing special"<<endl;
-
-
-
-
-}
+int length;
+cin >> length;
+vector<int> numbers;
+int temp;
+    for (int i = 0; i < length; ++i) {
+        cin>>temp;
+        numbers.push_back(temp);
+    }
+    for (int i = 0; i < length-1; ++i) {
+        cout<<numbers[i]<<", ";
+    }
+cout<< numbers[length-1]<<"."<<endl;
+return 0;}
